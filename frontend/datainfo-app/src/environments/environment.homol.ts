815ba6d5e840e15657e4a1da24e3611950e94dda
Josinaldo Júnior <josinaldo.junior@capgemini.com>
2021-05-10 09:14:52 -0300

    Ajuste de host da API

--- frontend/datainfo-app/src/environments/environment.homol.ts
+++ frontend/datainfo-app/src/environments/environment.homol.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   api: {
     protocol: 'http',
-    host: '192.168.99.100:8080',
+    // host: '192.168.99.100:8080',
+    host: 'localhost:8080',
     get url(){
       return `${this.protocol}://${this.host}/api`;
     }
